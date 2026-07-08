1727458d30a43ea412d65436da721ceac36304fa
Daekyeong, Park <pdk2037@gmail.com>
2026-07-08 17:38:46 +0900

    chore: 페이지에서 보여지는 섹션 순서 변경
    모집이 시작됨에 따라 사용자에게 모집 버튼을 우선적으로 보여주기 위해 순서를 수정합니다.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,9 +46,9 @@ export default function Root() {
           `}
         >
           <IntroduceSection />
-          <ProjectSection />
-          <PartSection />
           <RecruitSection />
+          <PartSection />
+          <ProjectSection />
         </div>
       </motion.main>
     </>
