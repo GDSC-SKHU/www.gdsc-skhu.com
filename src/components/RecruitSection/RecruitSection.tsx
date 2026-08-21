b1d956d553ea803d6fc53aa1c0c6191dc7d9ced5
Daekyeong, Park <pdk2037@gmail.com>
2026-08-21 14:55:23 +0900

    chore: 멤버 모집 버튼 활성화
    멤버 모집이 시작됨에 따라 모집 폼에 대한 버튼을 활성화합니다.
    각 파트명과 보여지는 순서를 5기에 맞게 수정합니다.

--- src/components/RecruitSection/RecruitSection.tsx
+++ src/components/RecruitSection/RecruitSection.tsx
@@ -14,16 +14,14 @@ import {
   sectionVerticalSpacing,
 } from '../../styles/constants';
 
-const WEB_RECRUIT_LINK =
-  'https://noble-animal-f7e.notion.site/GDG-on-Campus-SKHU-FE-WEB-Part-Recruiting-226dacc0346380a19e7cfeb0e509dabc';
-const MOBILE_RECRUIT_LINK =
-  'https://noble-animal-f7e.notion.site/GDG-on-Campus-SKHU-FE-Mobile-Part-Recruiting-226dacc0346380f78ebaeb0fc0506ad9';
-const SERVER_RECRUIT_LINK =
-  'https://noble-animal-f7e.notion.site/GDG-on-Campus-SKHU-Server-Part-Recruiting-226dacc034638014912ce71d847affa6';
+const FRONTEND_RECRUIT_LINK =
+  'https://butternut-yam-936.notion.site/GDG-on-Campus-SKHU-Frontend-Part-Recruiting-3c2f2706730e80cab65bff33f558da96';
+const BACKEND_RECRUIT_LINK =
+  'https://butternut-yam-936.notion.site/GDG-on-Campus-SKHU-Backend-Part-Recruiting-3bdf2706730e80c7bbc8fce9bc5fce70';
 const PM_RECRUIT_LINK =
-  'https://noble-animal-f7e.notion.site/GDG-on-Campus-SKHU-PM-Part-Recruiting-219dacc034638066805fe3267af2c967';
+  'https://butternut-yam-936.notion.site/GDG-on-Campus-SKHU-PM-Part-Recruiting-3c2f2706730e801b8d5dde7c2c7d11e9';
 const DESIGN_RECRUIT_LINK =
-  'https://noble-animal-f7e.notion.site/GDG-on-Campus-SKHU-Design-Part-Recruiting-226dacc0346380da84a3da41f713d46a';
+  'https://butternut-yam-936.notion.site/GDG-on-Campus-SKHU-Design-Part-Recruiting-3c2f2706730e8019a0a8de770ceed7cd';
 
 export default function RecruitSection() {
   return (
@@ -44,11 +42,10 @@ export default function RecruitSection() {
       </div>
 
       <div css={buttonWrapCss}>
-        {/* <RecruitAnchor href={PM_RECRUIT_LINK} text="🧭 PM 파트 멤버 지원하기" disable /> 
-        <RecruitAnchor href={DESIGN_RECRUIT_LINK} text="🎨 Design 파트 멤버 지원하기" disable />
-        <RecruitAnchor href={WEB_RECRUIT_LINK} text="🌎 Web 파트 멤버 지원하기" disable />
-        <RecruitAnchor href={MOBILE_RECRUIT_LINK} text="📱 Mobile 파트 멤버 지원하기" disable />
-        <RecruitAnchor href={SERVER_RECRUIT_LINK} text="🔧 Server 파트 멤버 지원하기" disable /> */}
+        <RecruitAnchor href={FRONTEND_RECRUIT_LINK} text="🌎 Frontend 파트 멤버 지원하기" />
+        <RecruitAnchor href={BACKEND_RECRUIT_LINK} text="🔧 Backend 파트 멤버 지원하기" />
+        <RecruitAnchor href={PM_RECRUIT_LINK} text="🧭 PM 파트 멤버 지원하기" />
+        <RecruitAnchor href={DESIGN_RECRUIT_LINK} text="🎨 Design 파트 멤버 지원하기" />
       </div>
     </motion.section>
   );
